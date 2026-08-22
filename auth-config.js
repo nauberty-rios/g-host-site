a89e38df7438d5064db53ca2d7f6fd7077be3831
NAUBERTY DE MORAES RIOS <naubertymoraes13@gmail.com>
2026-08-22 15:37:35 -0300

    Carregar controle de aparelhos nos painéis administrativos

--- auth-config.js
+++ auth-config.js
@@ -2,3 +2,11 @@ window.GHOST_AUTH_CONFIG = {
   apiBase: "https://g-host-secure.naubertymoraes13.workers.dev",
   inactivitySeconds: 900
 };
+
+if (!document.querySelector('script[data-ghost-device-access]')) {
+  const script = document.createElement("script");
+  script.src = "device-access.js";
+  script.defer = true;
+  script.dataset.ghostDeviceAccess = "1";
+  document.head.append(script);
+}
